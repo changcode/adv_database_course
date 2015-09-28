fd85d0c0108182643ac88a8bda329348003e55ca
Chang Shu <cshu100@gmail.com>
2015-09-28 23:41:47 +0000

    init website

---
+++ changshu.py
@@ -0,0 +1,35 @@
+from bottle import default_app, route, get, post, request, template
+
+@route('/')
+def hello_world():
+    return 'Hello from Bottle! ;)'
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    print (username)
+    print (password)
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+def check_login(username, password):
+    if username == 'cs':
+        return True
+    else:
+        return False
+
+application = default_app()
+
